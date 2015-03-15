e28116046879dc83e2b3ae8aa1f92b2da75d6e6b
Victor Schröder <schrodervictor@gmail.com>
2015-03-15 20:15:00 +0100

    Improves the pattern match for repeatHelloWorld/1

--- Functional-Programming/Introduction/Hello-World-N-Times/solution.erl
+++ Functional-Programming/Introduction/Hello-World-N-Times/solution.erl
@@ -2,16 +2,17 @@
 -export([main/0]).
 
 % Define the repeatHelloWorld function with two cases
-repeatHelloWorld(N)
-    % When N is zero, break the recursive execution
-    when N == 0 -> ok;
+% When N is zero, break the recursive execution
+repeatHelloWorld(0) -> ok;
 
+% When N is positive, print Hello World
 repeatHelloWorld(N)
-    % When N is positive, print Hello World
     when N > 0 ->
         io:format("~s~n", ["Hello World"]),
         % And call itself with N-1 (recursion)
         repeatHelloWorld(N-1).
+
+% When N is negative the call will fail
 
 main() ->
     % The expected input is one integer
