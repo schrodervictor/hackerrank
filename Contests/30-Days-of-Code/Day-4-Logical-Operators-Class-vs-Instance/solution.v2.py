eb4457e97ffda1fc5556765dd24fe9f4449fde96
Victor Schröder <schrodervictor@gmail.com>
2016-01-04 23:20:23 +0100

    Adds Day-4 problem (30-Days-of-Code) with solution (Python2)

---
+++ Contests/30-Days-of-Code/Day-4-Logical-Operators-Class-vs-Instance/solution.v2.py
@@ -0,0 +1,32 @@
+class Person:
+
+    def __init__(self, initialAge):
+        if initialAge < 0:
+            print "This person is not valid, setting age to 0."
+            age = 0
+        else:
+            age = initialAge
+        self.age = age
+
+    def amIOld(self):
+        if self.age < 13:
+            print "You are young."
+        elif self.age < 18:
+            print "You are a teenager."
+        else:
+            print "You are old."
+
+    def yearPasses(self):
+        self.age += 1
+
+# Runner code (given):
+
+T=int(raw_input())
+for i in range(0,T):
+    age=int(raw_input())
+    p=Person(age)
+    p.amIOld()
+    for j in range(0,3):
+        p.yearPasses();
+    p.amIOld();
+    print ""
